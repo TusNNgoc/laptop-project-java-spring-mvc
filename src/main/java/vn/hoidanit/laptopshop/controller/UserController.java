ae164c8f25dd666466b46eb3c8b736cb208138be
unknown <tus.nngoc@gmail.com>
2025-04-19 16:54:22 +0700

    To chuc file theo mo hinh mvc, viet thu mot userService va User controller don gian co dung mo hinh mvc va DI

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -7,33 +7,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.service.UserService;
 
-ko chay vi chua co
+//LOI: 1. code sai, 2. ko co view nen ko chay dc
+// @Controller
+// public class UserController(){
 
-@Controller
-public class UserController(){
+//     @RequestMapping("/")
+//     public String getHomePage() {
+//         return "Hello form Controller";
+//     }
+// }
 
-    @RequestMapping("/")
+//mo hinh chuan mvc
+@RestController
+public class UserController {
+
+    // ko nen dung vi sau test se kho [@AutoWired]
+
+    // DI
+    // khai bao 1 bien instance cua UserService to use in controller
+    private UserService userService;
+
+    // Neu muon tao UserController thi hay dua toi mot UserService truoc
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    // da viet theo mo hinh mvc, goi model
     public String getHomePage() {
-        return "Hello form Controller";
+        return this.userService.handleHello();
     }
 }
-
-// //mo hinh chuan mvc
-// @RestController
-// public class UserController {
-
-// // ko nen dung vi sau test se kho [@AutoWired]
-
-// //khai bao 1 bien instance cua UserService to use in controller
-// private UserService userService;
-
-// //Neu muon tao UserController thi hay dua toi mot UserService truoc
-// public UserController(UserService userService) {
-// this.userService = userService;
-// }
-
-// @GetMapping("/")
-// public String getHomePage() {
-// return this.userService.handleHello();
-// }
-// }
